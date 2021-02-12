19b02cef6914155cf19cb37d81d6af1b68bb39f9
jorgev259 <jorgev259@outlook.es>
2021-02-12 02:17:20 -0600

    rework gallery

--- src/js/Gallery.js
+++ src/js/Gallery.js
@@ -1,87 +1,73 @@
 import React from 'react'
-import { Modal, ModalBody, ModalFooter, Button } from 'reactstrap'
+import { Modal, ModalBody, ModalFooter, Button, Container, Col, Row } from 'reactstrap'
 import { navigate } from 'hookrouter'
 import Img from 'react-image'
 import LazyLoad from 'react-lazyload'
 import { stringToUrl } from './Strings'
+import classnames from 'classnames'
+import styles from '../css/gallery.module.scss'
 
-class ImgBG extends React.Component {
-  render () {
-    const path = this.props.includeType
-      ? `/img/thumbs/${this.props.game}/${this.props.device}/${this.props.type}/${this.props.image}`
-      : `/img/thumbs/${this.props.game}/${this.props.device}/${this.props.image}`
+function FullImg (props) {
+  const size = props.device === 'Desktop' ? 'lg' : 'sm'
+  const path = props.includeType
+    ? `/img/fullres/${props.game}/${props.device}/${props.type}/${props.modal}.jpg`
+    : `/img/fullres/${props.game}/${props.device}/${props.modal}.jpg`
 
-    const name = path.split('/').pop().split('.').shift()
-    return (
+  const pathBase = props.includeType
+    ? `/${stringToUrl[props.game]}/${stringToUrl[props.device]}/${stringToUrl[props.type]}`
+    : `/${stringToUrl[props.game]}/${stringToUrl[props.device]}`
 
-      <LazyLoad
-        key={this.props.image}
-        once
-      >
-        <Img
-          alt={this.props.image}
-          key={this.props.image}
-          draggable='false'
-          className={this.props.device}
-          src={path}
-          loader={<Img src={`/img/assets/loading_${this.props.device}.gif`} />}
-          onClick={() => navigate(`/${stringToUrl[this.props.game]}/${stringToUrl[this.props.device]}${this.props.includeType ? `/${stringToUrl[this.props.type]}/` : '/'}${name}`)}
-        />
-      </LazyLoad>
-
-    )
-  }
-}
-
-class FullImg extends React.Component {
-  render () {
-    const size = this.props.device === 'Desktop' ? 'lg' : 'sm'
-    const path = this.props.includeType
-      ? `/img/fullres/${this.props.game}/${this.props.device}/${this.props.type}/${this.props.modal}.jpg`
-      : `/img/fullres/${this.props.game}/${this.props.device}/${this.props.modal}.jpg`
-
-    const pathBase = this.props.includeType
-      ? `/${stringToUrl[this.props.game]}/${stringToUrl[this.props.device]}/${stringToUrl[this.props.type]}`
-      : `/${stringToUrl[this.props.game]}/${stringToUrl[this.props.device]}`
-
-    return (
-      <div className='bg-expand'>
-        <Modal isOpen={this.props.show} toggle={() => navigate(pathBase)} id='modalIMG' size={size} centered>
-          <ModalBody className='mb-0 p-0'>
+  return (
+    <div className='bg-expand'>
+      <Modal isOpen={props.show} toggle={() => navigate(pathBase)} id='modalIMG' size={size} centered>
+        <ModalBody className='mb-0 p-0'>
+          <LazyLoad key={props.modal} once>
             <Img
-              alt={this.props.modal}
+              alt={props.modal}
+              key={props.modal}
               style={{ width: '100%' }}
               draggable='false'
+              className={props.device}
               src={path}
-              loader={<Img src={`/img/assets/loading_${this.props.device}.gif`} />}
+              loader={<Img src={`/img/assets/loading_${props.device}.gif`} />}
             />
-          </ModalBody>
-          <ModalFooter>
-            <div className='mx-auto'>{this.props.modal}</div>
-            <div className='mx-auto'>
-              <a href={path} className='modal-btn btn-md text-center btn btn-outline-secondary' download>Download</a>
-              <Button outline className='btn-md ml-2 text-center modal-btn' data-dismiss='modal' type='button' onClick={() => navigate(pathBase)}>Close</Button>
-            </div>
-          </ModalFooter>
-
-        </Modal>
-      </div>
-    )
-  }
+          </LazyLoad>
+        </ModalBody>
+        <ModalFooter>
+          <div className='mx-auto'>{props.modal}</div>
+          <div className='mx-auto'>
+            <a href={path} className='modal-btn btn-md text-center btn btn-outline-secondary' download>Download</a>
+            <Button outline className='btn-md ml-2 text-center modal-btn' data-dismiss='modal' type='button' onClick={() => navigate(pathBase)}>Close</Button>
+          </div>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
 }
 
-export default class Gallery extends React.Component {
-  state = { modal: '' }
-  toggleModal = url => {
-    this.setState({ modal: url })
-  }
+function Thumb (props) {
+  const path = props.includeType
+    ? `/img/thumbs/${props.game}/${props.device}/${props.type}/${props.image}`
+    : `/img/thumbs/${props.game}/${props.device}/${props.image}`
 
-  render () {
-    return (
-      <div className='flex-fill'>
-        {this.props.modal ? <FullImg device={this.props.device} game={this.props.game} includeType={this.props.includeType} type={this.props.type} modal={this.props.modal} show={this.props.modal !== ''} toggle={this.toggleModal} /> : null}
-        {this.props.images.map(e => <ImgBG toggle={this.toggleModal} includeType={this.props.includeType} type={this.props.type} key={e} image={e} game={this.props.game} device={this.props.device} />)}
-      </div>
-    )
-  }
+  const name = path.split('/').pop().split('.').shift()
+
+  return (
+    <Col xs={props.device === 'Desktop' ? 12 : 6} sm={props.device === 'Desktop' ? 6 : 4} md={props.device === 'Desktop' ? 3 : 2} className={classnames(styles.thumb, styles[props.device], 'px-0')} onClick={() => navigate(`/${stringToUrl[props.game]}/${stringToUrl[props.device]}${props.includeType ? `/${stringToUrl[props.type]}/` : '/'}${name}`)}>
+      <div style={{ backgroundImage: `url("${path}")` }} />
+    </Col>
+  )
+}
+
+export default function Gallery (props) {
+  return (
+    <Container className='flex-fill content px-0' fluid>
+      {props.modal && <FullImg device={props.device} game={props.game} includeType={props.includeType} type={props.type} show={props.modal !== ''} modal={props.modal} />}
+      <Col>
+        <Row>
+          {props.images.map(i => <Thumb key={i} image={i} device={props.device} game={props.game} includeType={props.includeType} type={props.type} modal={props.modal} />)}
+        </Row>
+      </Col>
+    </Container>
+  )
 }
