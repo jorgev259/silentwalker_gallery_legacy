659312095993b73f98e82458f619900f8af34b81
jorgev259 <jorgev259@outlook.es>
2022-02-06 15:23:30 -0600

    Reduce size of donator button on mobile

--- src/js/Popup.jsx
+++ src/js/Popup.jsx
@@ -17,8 +17,8 @@ export default function Popup(){
     <div className={className(style.root, 'mx-auto')}>
       <div className={className(style.pop, 'mx-auto p-3')}>
         Help me to mantain the site ad-free by donating through
-        <Button color='primary' className={classNames('donate', style['btn-primary'])} href='https://paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WH26ZAAT9WRYU'>Paypal</Button> or <a href='https://www.buymeacoffee.com/silentwalker' target='_blank'><img height='36' style={{border:'0px',height:'36px'}} src='https://cdn.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
-        <Button color='primary' className={classNames('donate mx-2', style.donate)} onClick={donateSkip}>Understood</Button>
+        <Button color='primary' className={classNames('donate', style['btn-primary'], style.btn)} href='https://paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WH26ZAAT9WRYU'>Paypal</Button> or <a className={style.btn} href='https://www.buymeacoffee.com/silentwalker' target='_blank'><img height='36' style={{border:'0px',height:'36px'}} src='https://cdn.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+        <Button color='primary' className={classNames('donate mx-2', style.donate, style.btn)} onClick={donateSkip}>Understood</Button>
       </div>
     </div>
   ) : null
